b5378190a59cc0865d8ffd9fffdeac486430fe84
Jeffrey-sinos <jeffrey.ongicho@strathmore.edu>
2024-01-29 20:36:37 +0300

    Commit mass
    feat: add navbar
    
    add navbar and css to the navbar

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import './navbar.css'
+
+const Navbar = () => {
+    return(
+    <body>
+    <header className="header">
+        <nav className="navbar">
+        <div className="logo">
+      <h2 className="logo-heading">Jeffrey Ongicho</h2>
+        </div>
+    <div className="hamburger" id="hamburger">
+      <i className="fas fa-bars hamburger-icon" />
+      <i className="fas fa-times cross-icon" />
+    </div>
+    <div className="menu">
+      <ul className="menu-list">
+        <li className="menu-list-items">
+          <a className="links" href="#home">
+            Home
+          </a>
+        </li>
+        <li className="menu-list-items">
+          <a className="links" href="#projects">
+            Projects
+          </a>
+        </li>
+        <li className="menu-list-items">
+          <a className="links" href="#about">
+            About
+          </a>
+        </li>
+        <li className="menu-list-items">
+          <a className="links" href="#contact">
+            Contact Me
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</header>
+</body>
+    );
+}
+export default Navbar;
