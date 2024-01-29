f544ba26ddcf6a5f7c2d82d7ef67ce0e8e1144fd
Jeffrey-sinos <jeffrey.ongicho@strathmore.edu>
2024-01-29 22:27:30 +0300

    feat(style): add styling for entire project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './components/navbar/navbar.js';
 import Hero from './components/hero/hero.js';
 import Project from './components/projects/projects.js';
 import About from './components/about/about.js';
+import Contact from './components/contact/contacts.js';
 import './style.css';
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero/>
       <Project/>
       <About/>
+      <Contact/>
     </div>
   );
 }
